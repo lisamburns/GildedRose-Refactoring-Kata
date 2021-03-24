3e587fd13523f996a9052ef42fc4bb4b14a0b250
Lisa Burns <lisaburns@vmware.com>
2021-03-24 00:00:52 -0700

    sample solution

--- go/gilded-rose.go
+++ go/gilded-rose.go
@@ -5,54 +5,59 @@ type Item struct {
 	sellIn, quality int
 }
 
+const BRIE = "Aged Brie"
+const SULFURAS = "Sulfuras, Hand of Ragnaros"
+const BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert"
+
+var maxQuality = 50
+var minQuality = 0
+
+func (i *Item) newItemSellIn() int {
+	if i.name == SULFURAS {
+		return i.sellIn
+	} else {
+		return i.sellIn - 1
+	}
+}
+
+func (i *Item) newItemQuality() int {
+	switch name := i.name; name {
+	case SULFURAS:
+		return i.quality
+	case BRIE:
+		if i.sellIn < 0 {
+			return i.quality + 2
+		}
+		return i.quality + 1
+	case BACKSTAGE:
+		if i.sellIn > 10 {
+			return i.quality + 1
+		} else if i.sellIn > 5 {
+			return i.quality + 2
+		} else if i.sellIn > 0 {
+			return i.quality + 3
+		} else {
+			return 0
+		}
+	default:
+		if i.sellIn < 0 {
+			return i.quality - 2
+		}
+		return i.quality - 1
+	}
+}
+
 func UpdateQuality(items []*Item) {
 	for i := 0; i < len(items); i++ {
 
-		if items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
-			if items[i].quality > 0 {
-				if items[i].name != "Sulfuras, Hand of Ragnaros" {
-					items[i].quality = items[i].quality - 1
-				}
-			}
-		} else {
-			if items[i].quality < 50 {
-				items[i].quality = items[i].quality + 1
-				if items[i].name == "Backstage passes to a TAFKAL80ETC concert" {
-					if items[i].sellIn < 11 {
-						if items[i].quality < 50 {
-							items[i].quality = items[i].quality + 1
-						}
-					}
-					if items[i].sellIn < 6 {
-						if items[i].quality < 50 {
-							items[i].quality = items[i].quality + 1
-						}
-					}
-				}
-			}
-		}
+		items[i].sellIn = items[i].newItemSellIn()
+		items[i].quality = items[i].newItemQuality()
 
-		if items[i].name != "Sulfuras, Hand of Ragnaros" {
-			items[i].sellIn = items[i].sellIn - 1
+		if items[i].quality < minQuality {
+			items[i].quality = minQuality
 		}
-
-		if items[i].sellIn < 0 {
-			if items[i].name != "Aged Brie" {
-				if items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
-					if items[i].quality > 0 {
-						if items[i].name != "Sulfuras, Hand of Ragnaros" {
-							items[i].quality = items[i].quality - 1
-						}
-					}
-				} else {
-					items[i].quality = items[i].quality - items[i].quality
-				}
-			} else {
-				if items[i].quality < 50 {
-					items[i].quality = items[i].quality + 1
-				}
-			}
+		if items[i].quality > maxQuality {
+			items[i].quality = maxQuality
 		}
 	}
-
 }
