430613d4ce0b45d6afa8765276e13d2ee99840d5
Lisa Burns <lisaburns@vmware.com>
2021-03-23 23:07:41 -0700

    Add ginkgo tests for go language for all types except the backstage ticket

--- go/gilded-rose_test.go
+++ go/gilded-rose_test.go
@@ -1,15 +1,156 @@
 package main
 
-import "testing"
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
 
-func Test_Foo(t *testing.T) {
-	var items = []*Item{
-		&Item{"foo", 0, 0},
-	}
+var _ = Describe("UpdateQuality", func() {
+	var (
+		items   []*Item
+		quality int
+		sellIn  int
+	)
 
-	UpdateQuality(items)
+	Context("multiple items", func() {
+		JustBeforeEach(func() {
+			items = []*Item{
+				&Item{"item1", 10, 1},
+				&Item{"item2", 20, 2},
+			}
+			UpdateQuality(items)
+		})
 
-	if items[0].name != "fixme" {
-		t.Errorf("Name: Expected %s but got %s ", "fixme", items[0].name)
-	}
-}
+		It("updates the quality and sellIn date of each item", func() {
+			Expect(items[0].quality).To(Equal(0))
+			Expect(items[1].quality).To(Equal(1))
+			Expect(items[0].sellIn).To(Equal(9))
+			Expect(items[1].sellIn).To(Equal(19))
+		})
+
+		It("does not change the name", func() {
+			Expect(items[0].name).To(Equal("item1"))
+			Expect(items[1].name).To(Equal("item2"))
+		})
+	})
+
+	Context("regular items", func() {
+		BeforeEach(func() {
+			quality = 20
+			sellIn = 10
+		})
+		JustBeforeEach(func() {
+			items = []*Item{
+				&Item{"foo", sellIn, quality},
+			}
+			UpdateQuality(items)
+		})
+
+		Context("before sellIn date", func() {
+			BeforeEach(func() {
+				sellIn = 10
+			})
+			It("decreases 1 in quality and sellIn date", func() {
+				Expect(items[0].quality).To(Equal(19))
+				Expect(items[0].sellIn).To(Equal(9))
+			})
+		})
+
+		Context("after sellIn date", func() {
+			BeforeEach(func() {
+				sellIn = 0
+			})
+
+			It("decreases 1 in sellIn date, decreases 2 in quality", func() {
+				Expect(items[0].quality).To(Equal(quality - 2))
+				Expect(items[0].sellIn).To(Equal(sellIn - 1))
+			})
+		})
+
+		Context("quality is zero", func() {
+			BeforeEach(func() {
+				quality = 0
+			})
+
+			It("stays at zero quality", func() {
+				Expect(items[0].quality).To(Equal(0))
+			})
+		})
+	})
+	Context("Aged Brie items", func() {
+		BeforeEach(func() {
+			quality = 20
+			sellIn = 10
+		})
+		JustBeforeEach(func() {
+			items = []*Item{
+				&Item{"Aged Brie", sellIn, quality},
+			}
+			UpdateQuality(items)
+		})
+
+		Context("before sellIn date", func() {
+			BeforeEach(func() {
+				sellIn = 10
+			})
+			It("increases 1 in quality and decreases 1 in sellIn date", func() {
+				Expect(items[0].quality).To(Equal(quality + 1))
+				Expect(items[0].sellIn).To(Equal(sellIn - 1))
+			})
+		})
+
+		Context("after sellIn date", func() {
+			BeforeEach(func() {
+				sellIn = 0
+			})
+
+			It("increases 2 in quality and decreases 1 in sellIn date", func() {
+				Expect(items[0].quality).To(Equal(22))
+				Expect(items[0].sellIn).To(Equal(sellIn - 1))
+			})
+		})
+
+		Context("quality reaches max", func() {
+			BeforeEach(func() {
+				quality = 50
+			})
+
+			It("stays at max quality", func() {
+				Expect(items[0].quality).To(Equal(quality))
+			})
+		})
+	})
+	Context("Sulfuras items", func() {
+		BeforeEach(func() {
+			quality = 20
+			sellIn = 10
+		})
+		JustBeforeEach(func() {
+			items = []*Item{
+				&Item{"Sulfuras, Hand of Ragnaros", sellIn, quality},
+			}
+			UpdateQuality(items)
+		})
+
+		Context("before sellIn date", func() {
+			BeforeEach(func() {
+				sellIn = 10
+			})
+			It("stays the same in quality and sellIn date", func() {
+				Expect(items[0].quality).To(Equal(quality))
+				Expect(items[0].sellIn).To(Equal(sellIn))
+			})
+		})
+
+		Context("after sellIn date", func() {
+			BeforeEach(func() {
+				sellIn = 0
+			})
+
+			It("stays the same in quality and sellIn date", func() {
+				Expect(items[0].quality).To(Equal(quality))
+				Expect(items[0].sellIn).To(Equal(sellIn))
+			})
+		})
+	})
+})
